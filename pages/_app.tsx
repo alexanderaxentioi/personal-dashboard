d015b24f7ac6852ce1af746160937198504173f2
alexanderaxentioi <101560586+alexanderaxentioi@users.noreply.github.com>
2022-06-22 18:53:39 +0300

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { CacheProvider } from '@emotion/react'
+import { ThemeProvider, CssBaseline } from '@mui/material'
+
+import createEmotionCache from '../utility/createEmotionCache';
+import lightTheme from '../styles/theme/lightTheme';
+
+const clientSideEmotionCache = createEmotionCache()
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  const emotionCache = clientSideEmotionCache;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
 export default MyApp
---
+++ styles/theme/lightTheme.ts
@@ -0,0 +1,9 @@
+import { createTheme } from '@mui/material/styles';
+
+const lightTheme: any = createTheme({
+    palette: {
+        mode: 'light',
+    },
+});
+
+export default lightTheme;
