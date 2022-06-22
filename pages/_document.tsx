33501521ba478d01a5612d432eaede862dc91729
alexanderaxentioi <101560586+alexanderaxentioi@users.noreply.github.com>
2022-06-22 19:23:21 +0300

    fixed deploy

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -31,7 +31,7 @@ MyDocument.getInitialProps = async (ctx) => {
     /* eslint-disable */
     ctx.renderPage = () =>
         originalRenderPage({
-            enhanceApp: (App) =>
+            enhanceApp: (App: any) =>
                 function EnhanceApp(props) {
                     return <App emotionCache={cache} {...props} />;
                 },
