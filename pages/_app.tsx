4c913d2456406a0a8b0b070809c5022dc64270ab
alexanderaxentioi <101560586+alexanderaxentioi@users.noreply.github.com>
2022-06-22 19:17:30 +0300

    added dashboard mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { CacheProvider } from '@emotion/react'
+import { CacheProvider, EmotionCache } from '@emotion/react'
 import { ThemeProvider, CssBaseline } from '@mui/material'
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 import createEmotionCache from '../utility/createEmotionCache';
 import lightTheme from '../styles/theme/lightTheme';
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
 
 const clientSideEmotionCache = createEmotionCache()
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: MyAppProps) {
 
   const emotionCache = clientSideEmotionCache;
 
